cce6325bea2d984779f8feef6e4f87c644e57138
Gilgal Ansah <ansahgilgaljustice@gmail.com>
2019-01-20 18:39:11 +0000

    Update Day 3.py

--- Day 3.py
+++ Day 3.py
@@ -1,8 +1,10 @@
 #Modified Day 2 program. The sum is of only multiples of 3
+name = str.title(input("Enter your name to proceed: "))
+userNum= int(input("\nHello, "+name+". \n\nEnter an integer and let me give you the sum of all multiples of 3 from 1 to your number.\nEnter your number here: "))
+
 newList=[]
-print(newList)
-for x in range(1,16):
+for x in range(1,userNum+1):
 	if x%3==0:
 		newList.append(x)
-print (newList)
+
 print(sum(newList))
